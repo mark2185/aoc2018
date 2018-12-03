554dc74913c1edab13b6cbbdd08ccb708b5796a9
Luka Markusic <markusicluka@gmail.com>
2018-12-03 12:31:45 +0100

    Day 3 prettify

--- day03/1.cpp
+++ day03/1.cpp
@@ -1,24 +1,12 @@
 #include <iostream>
-#include <vector>
-#include <fstream>
-
-#include <boost/algorithm/string.hpp>
 
 int main(void) {
     char fabric[2000][2000] = {0};
     int cnt = 0;
-    int x, y, h, w;
+    int ID, x, y, h, w;
 
-    std::string line;
-    std::vector<std::string> results;
-
-    std::ifstream file("input");
-    while (getline(file, line)) {
-        boost::split(results, line, [](char c) { return c == ',' || c == ' ' || c == ':' || c == 'x';});
-        x = stoi(results[2]);
-        y = stoi(results[3]);
-        h = stoi(results[5]);
-        w = stoi(results[6]);
+    FILE *fp = fopen("input", "r");
+    while (fscanf(fp, "#%d @ %d,%d: %dx%d\n", &ID, &x, &y, &h, &w) == 5) {
         for (int i = x; i < x+h; ++i) {
             for (int j = y; j < y+w; ++j) {
                 if (fabric[i][j] == 1) {
--- day03/2.cpp
+++ day03/2.cpp
@@ -1,24 +1,12 @@
 #include <iostream>
-#include <vector>
-#include <fstream>
-
-#include <boost/algorithm/string.hpp>
 
 int main(void) {
     char fabric[2000][2000] = {0};
     int cnt = 0;
-    int x, y, h, w;
+    int ID, x, y, h, w;
 
-    std::string line;
-    std::vector<std::string> results;
-
-    std::ifstream file("input");
-    while (getline(file, line)) {
-        boost::split(results, line, [](char c) { return c == ',' || c == ' ' || c == ':' || c == 'x';});
-        x = stoi(results[2]);
-        y = stoi(results[3]);
-        h = stoi(results[5]);
-        w = stoi(results[6]);
+    FILE *fp = fopen("input", "r");
+    while (fscanf(fp, "#%d @ %d,%d: %dx%d\n", &ID, &x, &y, &h, &w) == 5) {
         for (int i = x; i < x+h; ++i) {
             for (int j = y; j < y+w; ++j) {
                 if (fabric[i][j] == 1) {
@@ -28,18 +16,9 @@ int main(void) {
             }
         }
     }
-    std::cout << cnt << std::endl;
 
-    file.clear();
-    file.seekg(0, std::ios::beg);
-
-    while (getline(file, line)) {
-        boost::split(results, line, [](char c) { return c == '#' || c == ',' || c == ' ' || c == ':' || c == 'x';});
-        int ID = stoi(results[1]);
-        x = stoi(results[3]);
-        y = stoi(results[4]);
-        h = stoi(results[6]);
-        w = stoi(results[7]);
+    fseek(fp, 0, SEEK_SET);
+    while (fscanf(fp, "#%d @ %d,%d: %dx%d\n", &ID, &x, &y, &h, &w) == 5) {
         bool flag = true;
         for (int i = x; i < x+h; ++i) {
             for (int j = y; j < y+w; ++j) {
